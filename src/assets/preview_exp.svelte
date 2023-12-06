7232835c0a76a3eaeea1ceeef47db9a393424945
theOriginalMe <timi.parkinson@gmail.com>
2023-12-06 07:57:53 -0800

    Added responsiveness for EXPERIENCE page

--- src/assets/preview_exp.svelte
+++ src/assets/preview_exp.svelte
@@ -1,5 +1,5 @@
 <div id={ $$props.id } class={$$props.class}>
-    <a href="https://blank.com/" target="_blank" rel="noopener noreferrer"></a>
+    <a class="link" href="https://blank.com/" target="_blank" rel="noopener noreferrer"></a>
     <div class="content-text">
         <h1 class="content-text-title">experience</h1>
         <p class="content-text-paragraph">Thank you for testing Portal Cards.</p>
