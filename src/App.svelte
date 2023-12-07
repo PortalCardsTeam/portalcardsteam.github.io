48698f7db4bc1079f2a15889a1ea18babda3f69b
portalcards <152940795+PortalCardsTeam@users.noreply.github.com>
2023-12-07 16:07:13 +0000

    Initial development of home page

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,21 @@
   import Experience from './lib/Experience.svelte';
   import Feedback from './lib/Feedback.svelte';
   import Tutorial from './lib/Tutorial.svelte';
+  import Home from './lib/Home.svelte';
+
+  let starting: boolean = false;
+  function begin() : void {
+    starting = true;
+  }
 </script>
 
 <main>
-  <div class="menu">
-    <Experience />
-    <Tutorial />
-    <Feedback />
-  </div>
+  <Home on:click={ begin }/>
+  {#if starting}
+    <div class="menu">
+      <Experience />
+      <Tutorial />
+      <Feedback />
+    </div>
+  {/if}
 </main>
---
+++ src/lib/Home.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+<div class="mug">
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+</div>
