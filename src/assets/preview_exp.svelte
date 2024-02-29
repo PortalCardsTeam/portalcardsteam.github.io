abeb5923a92eb0033fb5c3148dd2071e6555c50e
TimiParkinson <119539277+TimiParkinson@users.noreply.github.com>
2024-02-29 21:25:36 +0000

    Real link in use

--- src/assets/preview_exp.svelte
+++ src/assets/preview_exp.svelte
@@ -1,5 +1,5 @@
 <div id={ $$props.id } class={$$props.class}>
-    <a class="link" href="https://blank.com/" target="_blank" rel="noopener noreferrer"></a>
+    <a class="link" href="https://portal-cards.circle.so/join?invitation_token=490322c75818db0e5ac839ae956a9e4fa220e59c-bb34c7dd-f9b6-4dcd-a776-b1f9ec73504e" target="_blank" rel="noopener noreferrer"></a>
     <div class="content-text">
         <h1 class="content-text-title">experience</h1>
         <p class="content-text-paragraph">Thank you for testing Portal Cards.</p>
