9fcf219adc9d865b33547b24d3f340614ddbecc3
TimiParkinson <119539277+TimiParkinson@users.noreply.github.com>
2024-03-19 20:38:16 +0000

    Added tutorial videos

--- src/lib/Tutorial.svelte
+++ src/lib/Tutorial.svelte
@@ -4,11 +4,24 @@
     function fullScreen() {
         clicked = !clicked;
     }
+    let tutorialVideos: object = {
+        onboarding: '<iframe src="https://drive.google.com/file/d/1aEBl0hrRWiGLdlikrzekF1mg3spae8H3/preview" width="640" height="480" allow="autoplay"></iframe>',
+        navigation: '<iframe src="https://drive.google.com/file/d/15cYJqKZPdXsYbufRghcnbSg1tAK64DEg/preview" width="640" height="480" allow="autoplay"></iframe>',
+        "card completion": '<iframe src="https://drive.google.com/file/d/17xqOlfs1kVHbCIr-T4rOkW5ces7-7Bwb/preview" width="640" height="480" allow="autoplay"></iframe>'
+    };
 </script>
 
 <div class="card" on:click={fullScreen} class:clicked>
-    <Tutorial_SVG class="title" />
-    {#if clicked}
-        <h1><i>COMING SOON...</i></h1>
+    {#if !clicked}
+        <Tutorial_SVG class="title" />
+    {:else if clicked}
+        <div class=" video">
+            {#each Object.entries(tutorialVideos) as [title, video]}
+                <div id={ title } class="content-text">
+                    <h1 class="content-text-title">{ title }</h1>
+                    <p class="content-text-paragraph">{@html video }</p>
+                </div>
+            {/each} 
+        </div>
     {/if}
 </div>
